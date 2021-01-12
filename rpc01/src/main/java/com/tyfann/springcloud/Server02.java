19a7e2c3a6941ecf3bb8905e9bd0394e5c8e0a6a
tyfann <1342344773@qq.com>
2021-01-12 13:31:09 +0800

    curator create modify

--- rpc01/src/main/java/com/tyfann/springcloud/Server02.java
+++ rpc01/src/main/java/com/tyfann/springcloud/Server02.java
@@ -1,49 +1,23 @@
 package com.tyfann.springcloud;
 
 import com.tyfann.springcloud.controller.CuratorCreate;
-import com.tyfann.springcloud.entities.IUserService;
-import com.tyfann.springcloud.entities.User;
-import com.tyfann.springcloud.service.IUserServiceImpl02;
-
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.lang.reflect.Method;
 import java.net.InetAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 /**
  * @author tyfann
  * @date 2021/1/11 11:34 下午
  */
 public class Server02 {
-    private static boolean running = true;
+    static final int serviceSequence = 2;
     public static void main(String[] args) throws Exception {
         ServerSocket server = new ServerSocket(0,1, InetAddress.getByAddress(new byte[]{127,0,0,1}));
         int port = server.getLocalPort();
         CuratorCreate curatorCreate = new CuratorCreate();
         curatorCreate.create(port);
-        while(running){
-            Socket client = server.accept();
-            process(client);
-            client.close();
-        }
-        curatorCreate.delete(port);
+
+        curatorCreate.listen(server,serviceSequence);
         server.close();
-    }
-    public static void process(Socket socket) throws Exception {
-        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
-        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
 
-        //为了适应客户端通用化而做的改动
-        String methodName = ois.readUTF();
-        Class[] parameterTypes = (Class[]) ois.readObject();
-        Object[] parameters = (Object[]) ois.readObject();
-
-        IUserService service = new IUserServiceImpl02();//服务类型暂时还是写死的，不够灵活
-        Method method = service.getClass().getMethod(methodName, parameterTypes);
-        Object obj = method.invoke(service, parameters);
-        oos.writeObject(obj);
-        oos.flush();
     }
 }
--- rpc01/src/main/java/com/tyfann/springcloud/controller/CuratorCreate.java
+++ rpc01/src/main/java/com/tyfann/springcloud/controller/CuratorCreate.java
@@ -1,5 +1,9 @@
 package com.tyfann.springcloud.controller;
 
+import com.tyfann.springcloud.entities.IUserService;
+import com.tyfann.springcloud.service.IUserServiceImpl01;
+import com.tyfann.springcloud.service.IUserServiceImpl02;
+import lombok.SneakyThrows;
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
@@ -8,6 +12,12 @@ import org.apache.curator.retry.ExponentialBackoffRetry;
 import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.ZooDefs;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Method;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.util.Base64;
 
@@ -16,9 +26,11 @@ import java.util.Base64;
  * @date 2021/1/12 12:32 下午
  */
 public class CuratorCreate {
+    private static boolean running = true;
     CuratorFramework client;
     final String IP = "192.168.1.106:2181";
     public static final String zkServerPath = "127.0.0.1";
+
 
 
     public void create(int port) throws Exception {
@@ -35,8 +47,6 @@ public class CuratorCreate {
 
         createNode(port);
 
-        client.close();
-
     }
 
     public void createNode(int port) throws Exception {
@@ -47,17 +57,53 @@ public class CuratorCreate {
         client.checkExists()
                 .forPath("/rpc-payment-service");
         client.create()
-                .withMode(CreateMode.PERSISTENT)
+                .withMode(CreateMode.EPHEMERAL)
                 .withACL(ZooDefs.Ids.OPEN_ACL_UNSAFE)
                 .forPath("/rpc-payment-service/"+encodeIp,encodeIp.getBytes(StandardCharsets.UTF_8));
     }
 
-    public void delete(int port) throws Exception {
-        final Base64.Encoder encoder = Base64.getEncoder();
-        final String text = zkServerPath+":"+port;
-        final byte[] ipByte = text.getBytes(StandardCharsets.UTF_8);
-        final String encodeIp = encoder.encodeToString(ipByte);
-        client.delete()
-                .forPath("/rpc-payment-service/"+encodeIp);
+    public void listen(ServerSocket serverSocket,int serviceSequence) throws Exception {
+        Runtime.getRuntime().addShutdownHook(new Thread() {
+           @SneakyThrows
+           @Override
+           public void run() {
+               client.close();
+           }
+        });
+        while (running) {
+            Socket socketClient = serverSocket.accept();
+            process(socketClient,serviceSequence);
+            socketClient.close();
+        }
     }
+
+    public static void process(Socket socket,int serviceSequence) throws Exception {
+        ObjectInputStream ois = new ObjectInputStream(socket.getInputStream());
+        ObjectOutputStream oos = new ObjectOutputStream(socket.getOutputStream());
+
+        IUserService service;
+        //为了适应客户端通用化而做的改动
+        String methodName = ois.readUTF();
+        Class[] parameterTypes = (Class[]) ois.readObject();
+        Object[] parameters = (Object[]) ois.readObject();
+
+        switch (serviceSequence) {
+            case 1: {
+                service = new IUserServiceImpl01();//服务类型暂时还是写死的，不够灵活
+                break;
+            }
+            case 2: {
+                service = new IUserServiceImpl02();//服务类型暂时还是写死的，不够灵活
+                break;
+            }
+            default:
+                throw new IllegalStateException("Unexpected value: " + serviceSequence);
+        }
+
+        Method method = service.getClass().getMethod(methodName, parameterTypes);
+        Object Obj = method.invoke(service, parameters);
+        oos.writeObject(Obj);
+        oos.flush();
+    }
+
 }
